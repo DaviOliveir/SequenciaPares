393674e93d9c210a70a896ed78674afb9cb51359
Davi José Batista Lima de Oliveira <davibatistalimadeoliveira@gmail.com>
2022-11-04 21:27:39 -0300

    Sequencia Pares concluida

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Digite Somente o Número par de 0 a ?");
+
+int NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+
+if (NumeroDigitado >= 1)
+
+{   int numeroPar = 0;
+
+while (numeroPar <= NumeroDigitado)
+
+{ 
+    Console.Write($"{numeroPar}");
+    numeroPar += 2;
+
+}
+Console.WriteLine();
+
+} 
+
+else
+
+{
+
+    Console.WriteLine($" O Número {NumeroDigitado} não é positivo");
+}
